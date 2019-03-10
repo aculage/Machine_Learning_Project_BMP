f74f336ed3922d6a19a05be6d48f01a0c4eb830e
arseniculage <naoika@mail.ru>
2019-03-11 00:07:06 +0300

    Added perceptron class, refactored code. Comments show future intakes.

--- Machine_Learning_Project_BMP/Form1.cs
+++ Machine_Learning_Project_BMP/Form1.cs
@@ -11,15 +11,18 @@ using System.IO;
 
 namespace Machine_Learning_Project_BMP
 {
-    
+
+
     public partial class Form1 : Form
     {
         DirectoryInfo resfolder;
-
+        number_image[] images = new number_image[10];
         public Form1()
         {
-            
+
             InitializeComponent();
+
+            //Ловим ресурсы для обучения в листбокс
 
             FolderBrowserDialog pathselector = new FolderBrowserDialog();
             pathselector.ShowNewFolderButton = false;
@@ -27,21 +30,31 @@ namespace Machine_Learning_Project_BMP
             {
                 MessageBox.Show("Error");
             }
-              
+
             resfolder = new DirectoryInfo(pathselector.SelectedPath);
             FileInfo[] fileInfo = resfolder.GetFiles("*.bmp");
-            
-            foreach ( var file in fileInfo)
+
+            //Подгрузка весов перцептронов
+            /*for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    input[j,i]
+            }*/
+            //
+            foreach (var file in fileInfo)
             {
                 listBox1.Items.Add(file);
-                
+
             }
+
             TopMost = true;
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            //Распознавание элементов картинки, здесь нужно создать образ картинки
 
+            //
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -51,8 +64,31 @@ namespace Machine_Learning_Project_BMP
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            FileInfo straw =(FileInfo) listBox1.SelectedItem;
+            FileInfo straw = (FileInfo)listBox1.SelectedItem;
             pictureBox1.Image = (Bitmap)Image.FromFile(straw.FullName);
         }
+    }
+    public class number_image
+    {
+        public int[,] image;
+        public int[,] input;
+        public int floor = 10;
+
+        public int count_weight(int[,] cur_inp)
+        {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                    sum += image[i, j] * cur_inp[i, j];
+            }
+            return sum;
+        }
+        public number_image(int width, int height, int[,] image_in)
+        {
+            image = new int[width, height];
+            image = image_in;
+        }
+
     }
 }
