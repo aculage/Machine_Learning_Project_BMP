9f10955931f36df1c273d1173c287777faa7d902
arseniculage <naoika@mail.ru>
2019-03-17 16:01:08 +0300

    Рабочая версия

--- Machine_Learning_Project_BMP/Form1.Designer.cs
+++ Machine_Learning_Project_BMP/Form1.Designer.cs
@@ -30,13 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,6 +55,7 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(200, 200);
@@ -60,18 +64,9 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(313, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
-            // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(393, 218);
+            this.numericUpDown1.Location = new System.Drawing.Point(218, 217);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             9,
             0,
@@ -86,44 +81,97 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(218, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(89, 251);
+            this.listBox1.Size = new System.Drawing.Size(120, 199);
             this.listBox1.TabIndex = 5;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(393, 245);
+            this.button2.Location = new System.Drawing.Point(218, 243);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 42);
+            this.button2.Size = new System.Drawing.Size(119, 44);
             this.button2.TabIndex = 6;
             this.button2.Text = "Скорректировать";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // button3
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 293);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(500, 292);
-            this.textBox1.TabIndex = 7;
+            this.button3.Location = new System.Drawing.Point(123, 243);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 44);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Сохранить данные";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 26);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Очистить данные";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Нажать на пикчу для обучения ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(343, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(527, 512);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(218, 293);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 46);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Указать картинку";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(293, 26);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Картинку можно указать справа, \r\nкартинка для примера лежит в корневой папке прое" +
+    "кта";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(990, 600);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -134,11 +182,15 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Machine_Learning_Project_BMP/Form1.cs
+++ Machine_Learning_Project_BMP/Form1.cs
@@ -17,19 +17,28 @@ namespace Machine_Learning_Project_BMP
     {
         DirectoryInfo resfolder;
         number_image[] images = new number_image[10];
+        FolderBrowserDialog pathselector = new FolderBrowserDialog();
+        OpenFileDialog pictureselector = new OpenFileDialog();
+       
+        int found_number;
+        int[,] _current_image = new int[5, 8];
+
         public Form1()
         {
 
             InitializeComponent();
-
+           
             //Ловим ресурсы для обучения в листбокс
 
-            FolderBrowserDialog pathselector = new FolderBrowserDialog();
             pathselector.ShowNewFolderButton = false;
-            if (!(pathselector.ShowDialog() == DialogResult.OK))
+            pathselector.SelectedPath= Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(),@"..\..\" ) + "resfolder");
+            
+            
+          
+            /*if (!(pathselector.ShowDialog() == DialogResult.OK))
             {
                 MessageBox.Show("Error");
-            }
+            }*/
 
             resfolder = new DirectoryInfo(pathselector.SelectedPath);
             FileInfo[] fileInfo = resfolder.GetFiles("*.bmp");
@@ -52,6 +61,7 @@ namespace Machine_Learning_Project_BMP
                 images[i] = new number_image(5, 8, tmp_image);
                 //
             }
+            reader.Close();
 
             /*for (int j = 0; j < 8; j++)
             {
@@ -66,10 +76,66 @@ namespace Machine_Learning_Project_BMP
             }
 
             TopMost = true;
+           
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            
+            MessageBox.Show("Обучение, 25 прогонов по директории");
+            for (int kek=0; kek<=25; kek++)
+            {
+                
+                int count = 0;
+                int false_num = 0;
+                foreach (var file in listBox1.Items)
+                {
+                    count++;
+
+                    FileInfo straw = (FileInfo)file;
+                    if (straw == null) { MessageBox.Show("Выбери картинку дебил"); }
+                    else
+                    {
+                        Bitmap current_image = (Bitmap)Image.FromFile(straw.FullName);
+
+                        int maximum = -500;
+                        for (int z = 0; z < 10; z++)
+                        {
+                            for (int i = 0; i < current_image.Height; i++)
+                                for (int j = 0; j < current_image.Width; j++)
+                                {
+                                    Color color = current_image.GetPixel(j, i);
+                                    if ((color.R < 225) && (color.G < 225) && (color.B < 225))
+                                        _current_image[j, i] = 1;
+                                    else _current_image[j, i] = 0;
+                                }
+                        }
+                        for (int i = 0; i < 10; i++)
+                        {
+
+                            if (images[i].count_weight(_current_image) > maximum)
+                            {
+                                maximum = images[i].count_weight(_current_image);
+                                found_number = i;
+                            }
+                        }
+                        FileInfo file_ = (FileInfo)file;
+                        string rightnum = "";
+                        rightnum += file_.Name[0];
+                        if (!(found_number == int.Parse(rightnum)))
+                        //MessageBox.Show("Определена цифра " + found_number);
+                        {
+                            false_num++;
+                            images[found_number].correct_false(_current_image);
+                            images[int.Parse(rightnum)].correct_true(_current_image);
+                        }
+
+                    }
+                    
+                }
+                
+                richTextBox1.Text += "Процент ошибок " + kek + " ого прохода: " + Math.Truncate((double)false_num / (double)count * 100.0)  + "% \n ";
+            }
            
         }
 
@@ -80,7 +146,7 @@ namespace Machine_Learning_Project_BMP
             else
             {
                 Bitmap current_image = (Bitmap)Image.FromFile(straw.FullName);
-                int[,] _current_image = new int[5, 8];
+               
                 int maximum = -500;
                 for (int z = 0; z < 10; z++)
                 {
@@ -88,10 +154,21 @@ namespace Machine_Learning_Project_BMP
                         for (int j = 0; j < current_image.Width; j++)
                         {
                             Color color = current_image.GetPixel(j, i);
-                            if((color.R<225)&&(color.G<225)&&(color.B<225))
+                            if ((color.R < 225) && (color.G < 225) && (color.B < 225))
                                 _current_image[j, i] = 1;
+                            else _current_image[j, i] = 0;
                         }
                 }
+                for (int i=0; i<10; i++)
+                {
+                  
+                    if(images[i].count_weight(_current_image)>maximum)
+                    {
+                        maximum = images[i].count_weight(_current_image);
+                        found_number = i;
+                    }
+                }
+                MessageBox.Show("Определена цифра " + found_number);
 
             }
         }
@@ -104,6 +181,66 @@ namespace Machine_Learning_Project_BMP
 
         private void button2_Click(object sender, EventArgs e)
         {
+            images[found_number].correct_false(_current_image);
+            images[(int)numericUpDown1.Value].correct_true(_current_image);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            StreamWriter writer = new StreamWriter (pathselector.SelectedPath + "/weights.txt");
+            foreach (var img in images)
+            {
+                writer.WriteLine(num);
+                num++;
+                for (int j = 0; j < 8; j++)
+                {
+
+                    
+                    for (int k = 0; k < 5; k++)
+                        writer.Write(img.image[k,j]+" ");
+                    writer.WriteLine();
+                }
+
+            }
+            writer.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            File.Delete(pathselector.SelectedPath + "/backup.txt");
+            File.Copy(pathselector.SelectedPath + "/weights.txt", pathselector.SelectedPath + "/backup.txt");
+            int num = 0;
+            StreamWriter writer = new StreamWriter(pathselector.SelectedPath + "/weights.txt");
+            foreach (var img in images)
+            {
+                writer.WriteLine(num);
+                num++;
+                for (int j = 0; j < 8; j++)
+                {
+
+
+                    for (int k = 0; k < 5; k++)
+                        writer.Write(0 + " ");
+                    writer.WriteLine();
+                }
+
+            }
+            writer.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!(pictureselector.ShowDialog() == DialogResult.OK))
+            {
+                MessageBox.Show("Error");
+            }
+            pictureBox1.Image = (Bitmap)Image.FromFile(pictureselector.FileName);
 
         }
     }
@@ -122,6 +259,26 @@ namespace Machine_Learning_Project_BMP
                     sum += image[i, j] * cur_inp[i, j];
             }
             return sum;
+        }
+        public void correct_false(int [,] cur_inp)
+        {
+            for (int i=0;i<5;i++)
+            {
+                for (int j=0;j<8;j++)
+                {
+                    image[i, j] -= cur_inp[i,j];
+                }
+            }
+        }
+        public void correct_true(int [,] cur_inp)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    image[i, j] += cur_inp[i, j];
+                }
+            }
         }
         public number_image(int width, int height, int[,] image_in)//создание класса перцептрона
         {
